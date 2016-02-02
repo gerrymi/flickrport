693cd7aeb371ed178b2aaffb735769b5c589662e
Gerrymi Bernardo <gerrymibernardo@gmail.com>
2016-02-02 15:54:11 -0800

    Initial commit, working username to user_id translation

---
+++ app.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var ports = require('./routes/p');
+
+var Flickr = require("flickrapi"),
+    flickrOptions = {
+      api_key: "9925e9fc9654b7141240423e98da68e6",
+      secret: "c6cd6aa6931ebfdd"
+    };
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log ('app working')
+
+app.use('/', routes);
+app.use('/p', ports);
+
+// Convert Flickr username to userid
+app.post('/', function(req, res) {
+  var username = req.body.name
+  Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+    flickr.people.findByUsername({
+      username: username,
+      format: JSON
+    }, function(err, result) {
+      console.log (result.user.id)
+    });
+  });
+});
+
+// app.post('/', function(req, res) {
+//   var username = req.body.name
+//   Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+//     flickr.people.findByUsername({
+//       username: username,
+//       format: JSON
+//     }, function(err, result) {
+//       console.log (result.user.id + ' ' +username)
+//       flickr.photosets.getList({
+//         user_id: result.user.id,
+//         format: JSON
+//       }, function(err, result) {
+//         console.log (result.photosets.photoset)
+//       });
+//     });
+//   });
+// });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;

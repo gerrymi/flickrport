c6e87a6ab59563f7f88b987a29db5c92ea4da568
Gerrymi Bernardo <gerrymibernardo@gmail.com>
2016-02-04 16:13:15 -0800

    need a way to postpone json write until loop is over

--- routes/p.js
+++ routes/p.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var jsonfile = require('jsonfile')
+
 var Flickr = require("flickrapi"),
     flickrOptions = {
       api_key: "9925e9fc9654b7141240423e98da68e6",
@@ -17,22 +19,35 @@ router.get('/', function(req, res, next) {
       user_id: user_id,
       format: JSON
     }, function(err, result) {
+      console.log ("Recieved Photoset List")
       for (var i in result.photosets.photoset) {
         flickr.photosets.getPhotos({
           user_id: user_id,
           photoset_id: result.photosets.photoset[i].id,
           format: JSON
         }, function(err, result2) {
-          // for (var j in result2.photoset.photo) {
-          for (j=0; j<2; j++) {
-            var farm = result2.photoset.photo[j].farm
-            var server = result2.photoset.photo[j].server
-            var id = result2.photoset.photo[j].id
-            var secret = result2.photoset.photo[j].secret
-            var photo = '{"id": '+id+', "secret": '+secret+', "server": '+server+', "farm": '+farm+', "photoset_id": '+result2.photoset.id+', "photoset_title": '+result2.photoset.title+' }'
+          console.log ("Recieved Photos From Photoset: "+result2.photoset.id)
+          for (var j in result2.photoset.photo) {
+          // for (j=0; j<1; j++) {
+            var farm = '"'+result2.photoset.photo[j].farm+'"'
+            var server = '"'+result2.photoset.photo[j].server+'"'
+            var id = '"'+result2.photoset.photo[j].id+'"'
+            var secret = '"'+result2.photoset.photo[j].secret+'"'
+            var photoset_id = '"'+result2.photoset.id+'"'
+            var photoset_title = '"'+result2.photoset.title+'"'
+
+            var photo = '{"id": '+id+', "secret": '+secret+', "server": '+server+', "farm": '+farm+', "photoset_id": '+photoset_id+', "photoset_title": '+photoset_title+' }'
             gallery.push (photo)
+            console.log ("Recieved Photo: "+photoset_id)
+            var galleryString = gallery.toString();
             // gallery += '<img class="hidden '+result2.photoset.id+'" src="https://farm' + farm + '.staticflickr.com/' + server + '/' + id + '_' + secret + '_' + 'm' + '.jpg" alt="">' 
-            console.log (gallery)
+            var galleryParse = '{ "allPhotos": ['+galleryString+' ]}'
+            var file = './public/data/'+username+'.json'
+            var obj = JSON.parse(galleryParse)
+            // console.log (obj)
+            jsonfile.writeFile(file, obj, function (err) {
+              console.error(err)
+            })
           }
         });
       }

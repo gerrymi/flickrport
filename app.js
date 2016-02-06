b1eeeb7124da3328eadac3823c68a8e113ccea33
Gerrymi Bernardo <gerrymibernardo@gmail.com>
2016-02-06 01:28:43 -0800

    json file loads when not on debug mode bin/www, all photos loading, no infinite scroll loading, no transitions

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-process.env.PWD = process.cwd()
-
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -12,7 +9,6 @@ var routes = require('./routes/index');
 var ports = require('./routes/p');
 
 var app = express();
-app.use(express.static(process.env.PWD + '/htdocs'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +32,8 @@ app.post('/', function(req, res) {
   var username = req.body.name
   res.redirect('/p?username='+username)
 });
+
+app.listen(3000);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

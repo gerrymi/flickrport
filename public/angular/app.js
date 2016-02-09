5f67193a26673f80d763e3c66f127e788ffddbb7
Gerrymi Bernardo <gerrymibernardo@gmail.com>
2016-02-08 17:54:13 -0800

    anchorScroll enabled, need smooth scroll

--- public/angular/app.js
+++ public/angular/app.js
@@ -2,3 +2,45 @@ var app = angular.module("flickrPort", ["infinite-scroll", "ngAnimate"]);
 
 angular.module('infinite-scroll').value('THROTTLE_MILLISECONDS', 3000)
 
+app.controller('galleryCtrl', function($scope, $location, $anchorScroll, flickrPort) {
+	$scope.flickrPort = new flickrPort();
+	$scope.scrollTo = function(x) {
+      var newHash = 'photoset-' + x;
+            if ($location.hash() !== newHash) {
+              // set the $location.hash to `newHash` and
+              // $anchorScroll will automatically scroll to it
+              $location.hash('photoset-' + x);
+            } else {
+              // call $anchorScroll() explicitly,
+              // since $location.hash hasn't changed
+              $anchorScroll();
+            }
+      console.log(newHash)
+   }
+});
+
+app.factory('flickrPort', function($http) {
+	var flickrPort = function() {
+	  this.photoSets = [];
+	  this.busy = false;
+	};
+	var loaded = 0
+
+    flickrPort.prototype.nextPage = function() {
+		if (this.busy) return;
+		this.busy = true;
+
+      	$http.get("https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=9925e9fc9654b7141240423e98da68e6&user_id="+user_id+"&format=json&nojsoncallback=1").then(function(res){
+	        photoSetList = res.data.photosets.photoset
+	        for (var i = loaded; i < loaded+3; i++) {
+	          	$http.get("https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=9925e9fc9654b7141240423e98da68e6&photoset_id="+photoSetList[i].id+"&user_id="+user_id+"&format=json&nojsoncallback=1").then(function(res){
+		            photos = res.data.photoset
+		            this.photoSets.push(photos);
+		            loaded += 1
+	          	}.bind(this));
+	        }
+		   	this.busy = false;
+	    }.bind(this));
+	};
+    return flickrPort;
+});

2ab7a9ae8e3f8fee909910ff2dc489a8df88d566
Gerrymi Bernardo <gerrymibernardo@gmail.com>
2016-02-10 02:38:45 -0800

    styled index for production

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'fickrPort' });
+  res.render('index', { title: 'flickrPort' });
 });
+
 
 module.exports = router;

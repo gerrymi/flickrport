ea965e3c18138b366519a21479cd329d5a039505
Gerrymi Bernardo <gerrymibernardo@gmail.com>
2016-02-26 18:00:41 -0800

    added modal

--- public/angular/app.js
+++ public/angular/app.js
@@ -22,34 +22,33 @@ app.controller('galleryCtrl', function($scope, $routeParams, $location, $anchorS
       console.log(newHash)
    }
    $scope.username = $routeParams.username;
+   $scope.photoset = $routeParams.photoset_id;2
 })
 
 app.factory('flickrPort', function($http, $routeParams) {
 	var flickrPort = function() {
-		$http.get("https://api.flickr.com/services/rest/?method=flickr.people.findByUsername&api_key=9925e9fc9654b7141240423e98da68e6&username="+$routeParams.username+"&format=json&nojsoncallback=1").then(function(res){
-			user_id = res.data.user.id;
-		}.bind(this));
 		this.photoSets = [];
 		this.busy = false;
-
 	};
 	var loaded = 0
-	
+
     flickrPort.prototype.nextPage = function() {
 		if (this.busy) return;
 		this.busy = true;
-
-      	$http.get("https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=9925e9fc9654b7141240423e98da68e6&user_id="+this.user_id+"&format=json&nojsoncallback=1").then(function(res){
-	        photoSetList = res.data.photosets.photoset
-	        for (var i = loaded; i < loaded+3; i++) {
-	          	$http.get("https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=9925e9fc9654b7141240423e98da68e6&photoset_id="+photoSetList[i].id+"&user_id="+user_id+"&format=json&nojsoncallback=1").then(function(res){
-		            photos = res.data.photoset 
-		            this.photoSets.push(photos);
-	          	}.bind(this));
-	        }
-	        loaded += 3
-		   	this.busy = false;
-	    }.bind(this));
+		$http.get("https://api.flickr.com/services/rest/?method=flickr.people.findByUsername&api_key=9925e9fc9654b7141240423e98da68e6&username="+$routeParams.username+"&format=json&nojsoncallback=1").then(function(res){
+			user_id = res.data.user.id;
+	      	$http.get("https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=9925e9fc9654b7141240423e98da68e6&user_id="+user_id+"&format=json&nojsoncallback=1").then(function(res){
+		        photoSetList = res.data.photosets.photoset
+		        for (var i = loaded; i < loaded+3; i++) {
+		          	$http.get("https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=9925e9fc9654b7141240423e98da68e6&photoset_id="+photoSetList[i].id+"&user_id="+user_id+"&format=json&nojsoncallback=1").then(function(res){
+			            photos = res.data.photoset 
+			            this.photoSets.push(photos);
+		          	}.bind(this));
+		        }
+		        loaded += 3
+			   	this.busy = false;
+		    }.bind(this));
+		}.bind(this));
 	};
     return flickrPort;
 });

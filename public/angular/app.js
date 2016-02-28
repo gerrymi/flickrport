3983a1630aef6f94f58a9c599f800a3dd4b855a0
Gerrymi Bernardo <gerrymibernardo@gmail.com>
2016-02-27 18:08:21 -0800

    added flex-slider + flex-slider custom styling

--- public/angular/app.js
+++ public/angular/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("flickrPort", ["infinite-scroll", "ngAnimate", "ui.router", "angular-click-outside"]);
+var app = angular.module("flickrPort", ["infinite-scroll", "ngAnimate", "ui.router", "angular-click-outside", "angular-flexslider"]);
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('no-user', {
@@ -18,6 +18,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
     })
   $urlRouterProvider.otherwise("/");
 });
+
 app.controller('galleryCtrl', function($window, $scope, $rootScope, $stateParams, $http, $location, $anchorScroll, flickrPort) {
 	$window.document.title = "flickrPort: " + $stateParams.username
 	$scope.flickrPort = new flickrPort();
@@ -28,24 +29,30 @@ app.controller('galleryCtrl', function($window, $scope, $rootScope, $stateParams
             } else {
               $anchorScroll();
             }
-   }
-   $rootScope.username = $stateParams.username;
-   $rootScope.photoset_id = $stateParams.photoset_id;
-   $http.get("https://api.flickr.com/services/rest/?method=flickr.people.findByUsername&api_key=9925e9fc9654b7141240423e98da68e6&username="+$stateParams.username+"&format=json&nojsoncallback=1").then(function(res){
-			user_id = res.data.user.id;
-			$http.get("https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=9925e9fc9654b7141240423e98da68e6&photoset_id="+$stateParams.photoset_id+"&user_id="+user_id+"&format=json&nojsoncallback=1").then(function(res){
-			$scope.photos = res.data.photoset
-   		});
-   });
-   $scope.go = function() {
-     $location.path( $stateParams.username );
-   };
-   $scope.scrollOff = function(){ 
-   		$rootScope.scroll = "modal-no-scroll";
-   }; 
-   $scope.scrollOn = function(){ 
-   		$rootScope.scroll = "modal-scroll"
-   }; 
+  }
+  $rootScope.username = $stateParams.username;
+  $rootScope.photoset_id = $stateParams.photoset_id;
+  $http.get("https://api.flickr.com/services/rest/?method=flickr.people.findByUsername&api_key=9925e9fc9654b7141240423e98da68e6&username="+$stateParams.username+"&format=json&nojsoncallback=1").then(function(res){
+  	user_id = res.data.user.id;
+  	$http.get("https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=9925e9fc9654b7141240423e98da68e6&photoset_id="+$stateParams.photoset_id+"&user_id="+user_id+"&format=json&nojsoncallback=1").then(function(res){
+  	  $scope.photos = res.data.photoset
+  		});
+  });
+  $scope.go = function() {
+   $location.path( $stateParams.username );
+  };
+  $scope.scrollOff = function(){ 
+  		$rootScope.scroll = "modal-no-scroll";
+  }; 
+  $scope.scrollOn = function(){ 
+  		$rootScope.scroll = "modal-scroll"
+  };
+  $scope.slides = [
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+  ];
 })
 app.factory('flickrPort', function($http, $stateParams) {
 	var flickrPort = function() {
@@ -54,11 +61,11 @@ app.factory('flickrPort', function($http, $stateParams) {
 	};
 	var loaded = 0
 
-    flickrPort.prototype.nextPage = function() {
+  flickrPort.prototype.nextPage = function() {
 		if (this.busy) return;
 		this.busy = true;
-		$http.get("https://api.flickr.com/services/rest/?method=flickr.people.findByUsername&api_key=9925e9fc9654b7141240423e98da68e6&username="+$stateParams.username+"&format=json&nojsoncallback=1").then(function(res){
-			user_id = res.data.user.id;
+		  $http.get("https://api.flickr.com/services/rest/?method=flickr.people.findByUsername&api_key=9925e9fc9654b7141240423e98da68e6&username="+$stateParams.username+"&format=json&nojsoncallback=1").then(function(res){
+        user_id = res.data.user.id;
 	      	$http.get("https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=9925e9fc9654b7141240423e98da68e6&user_id="+user_id+"&format=json&nojsoncallback=1").then(function(res){
 		        photoSetList = res.data.photosets.photoset
 		        for (var i = loaded; i < loaded+3; i++) {
